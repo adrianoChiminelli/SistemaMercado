28148746e1de32dffd1da6c0aa147ed75c8d7795
Adriano Chiminelli <adriano.reserva@outlook.com>
2022-07-27 21:54:50 -0300

    Criado um utilitário para validação de CPF

---
+++ utilitarioCPF/src/main/java/com/mycompany/utilitariocpf/UtilCPF.java
@@ -0,0 +1,107 @@
+package com.mycompany.utilitariocpf;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import javax.swing.text.MaskFormatter;
+
+public class UtilCPF {
+
+    /**
+     * Método que gera um CPF aleatório válido
+     *
+     * @return Retorna uma String que contém um CPF, sem pontos e hífem.
+     */
+    public String geraCPFAleatorio() {
+
+        ArrayList<Integer> listaNumAleatorio = new ArrayList<>();
+        for (int i = 0; i <= 8; i++) {
+            listaNumAleatorio.add(geraNumeroAleatorio());
+        }
+
+        String cpf = geraDigitosVerificador(listaNumAleatorio);
+
+        return cpf;
+    }
+
+    /**
+     * Método que verifica se um CPF é válido
+     *
+     * @param cpf CPF informado para validação
+     * @return Retorna um boolean informando se o CPF é válido ou não
+     */
+    public boolean validaCPF(String cpf) {
+        if (cpf.contains(".")) {
+            cpf = removeMascara(cpf);
+        }
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        ArrayList<Integer> noveDigitos = new ArrayList<>();
+
+        for (int i = 0; i < (cpf.length() - 2); i++) {
+            noveDigitos.add(Character.getNumericValue(cpf.charAt(i)));
+        }
+
+        String testaCPF = geraDigitosVerificador(noveDigitos);
+
+        return cpf.equals(testaCPF);
+    }
+
+    public String formatarCPF(String cpf) {
+        if (cpf.contains(".")) {
+            return cpf;
+        }
+        String cpfFormatado = "";
+        try {
+            MaskFormatter mf = new MaskFormatter("###.###.###-##");
+            mf.setValueContainsLiteralCharacters(false);
+            cpfFormatado = mf.valueToString(cpf);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return cpfFormatado;
+    }
+
+    private String geraDigitosVerificador(ArrayList<Integer> listaDigitos) {
+
+        int peso = 10;
+        for (int i = 0; i < 2; i++) {
+            if (i == 1) {
+                peso = 11;
+            }
+
+            int somaDigitosComPeso = 0;
+
+            for (int Digito : listaDigitos) {
+                somaDigitosComPeso = somaDigitosComPeso + (Digito * peso);
+                peso--;
+            }
+            int restoDivisao = (somaDigitosComPeso % 11);
+
+            if (restoDivisao < 2) {
+                listaDigitos.add(0);
+            } else {
+                listaDigitos.add((11 - restoDivisao));
+            }
+        }
+        String textoCPF = "";
+        for (int item : listaDigitos) {
+            textoCPF += item;
+        }
+
+        return textoCPF;
+    }
+
+    private int geraNumeroAleatorio() {
+        int numero = (int) (Math.random() * 10);
+        return numero;
+    }
+
+    private String removeMascara(String cpf) {
+        return cpf.replaceAll("\\D", "");
+    }
+
+}
